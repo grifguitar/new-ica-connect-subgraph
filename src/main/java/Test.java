b4a7dd71d72473935e1adc329a718780fa1379c6
Grigoriy Khlytin <xlootin@niuitmo.ru>
2022-06-14 23:08:40 +0300

    update

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+import drawing.DrawAPI;
+
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 0; i++) {
+            Main main = new Main("a" + i + "_big_test_05");
+            main.run(null);
+        }
+        DrawAPI.run();
+    }
+}
